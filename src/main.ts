dae8feec220bc880ba6726ded68e87c2eb755c81
Jeremy-Camus-Varela-Aiquia <jeremy@aiquia.com>
2023-12-21 18:30:31 -0300

    feat: validation pipe has been added to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT') ?? 8080;
   await app.listen(port);
